3dde142f0bc1de58e9aafc43ef7135b88ce3267f
thsWebstorm03 <webstorm03@gmail.com>
2023-12-21 21:44:54 +0800

    mainly complete

---
+++ helper.js
@@ -0,0 +1,223 @@
+import fetch from "node-fetch";
+import dotenv from "dotenv";
+dotenv.config();
+
+const token = process.env.TELEGRAM_BOT_TOKEN;
+const apiKey = process.env.TRELLO_API_KEY;
+const apiToken = process.env.TRELLO_API_TOKEN;
+// const boardId = process.env.TRELLO_BOARD_ID;
+
+export const getBoards = async () => {
+   const apiURL = `https://api.trello.com/1/members/me/boards?key=${apiKey}&token=${apiToken}`;
+   try {
+      const response = await fetch(apiURL, { method: "GET" });
+
+      if (response.ok) {
+         const responseBody = await response.json();
+         return responseBody;
+      } else {
+         console.error("Error creating Trello board:", response.statusText);
+         return false;
+      }
+   } catch (error) {
+      console.error("Error:", error);
+      return false;
+   }
+};
+
+export const getBoardIdByName = async (boardName) => {
+   const boards = await getBoards();
+   const matchingBoard = boards.find((board) => board.name === boardName);
+
+   if (matchingBoard) {
+      console.log(`Board "${boardName}" found with ID: ${matchingBoard.id}`);
+      return matchingBoard.id;
+   } else {
+      console.log(`Board "${boardName}" not found.`);
+      return null;
+   }
+};
+
+export const getBoardLists = async (boardId) => {
+   const listsUrl = `https://api.trello.com/1/boards/${boardId}/lists?key=${apiKey}&token=${apiToken}`;
+
+   try {
+      const response = await fetch(listsUrl);
+      const lists = await response.json();
+
+      console.log("Lists for the board:");
+      lists.forEach((list) => {
+         console.log(`- ${list.name} (ID: ${list.id})`);
+      });
+
+      return lists;
+   } catch (error) {
+      console.error("Error:", error);
+      return null;
+   }
+};
+
+export const getBoardListIDByName = async (boardId, listName) => {
+   const lists = await getBoardLists(boardId);
+   const matchinglist = lists.find((list) => list.name === listName);
+
+   if (matchinglist) {
+      console.log(`List "${listName}" found with ID: ${matchinglist.id}`);
+      return matchinglist.id;
+   } else {
+      console.log(`List "${listName}" not found.`);
+      return null;
+   }
+}
+
+export const getCardlist = async (id) => {
+   const apiURL = `https://api.trello.com/1/cards/${id}/list?key=${apiKey}&token=${apiToken}`;
+   try {
+      const response = await fetch(apiURL, { method: "GET" });
+
+      if (response.ok) {
+         const responseBody = await response.json();
+         return responseBody;
+      } else {
+         console.error("Error creating Trello board:", response.statusText);
+         return false;
+      }
+   } catch (error) {
+      console.error("Error:", error);
+      return false;
+   }
+};
+
+export const getFileUrl = async (fileId) => {
+   const apiUrl = `https://api.telegram.org/bot${token}/getFile?file_id=${fileId}`;
+
+   try {
+      const response = await fetch(apiUrl);
+      const data = await response.json();
+
+      if (data.ok) {
+         const fileUrl = `https://api.telegram.org/file/bot${token}/${data.result.file_path}`;
+         return fileUrl;
+      } else {
+         console.error("Error getting file information:", data.description);
+         return null;
+      }
+   } catch (error) {
+      console.error("Error:", error);
+      return null;
+   }
+};
+
+export const addBoard = async (name) => {
+   const apiURL = `https://api.trello.com/1/boards?key=${apiKey}&token=${apiToken}&name=${name}`;
+   try {
+      const response = await fetch(apiURL, { method: "POST" });
+
+      if (response.ok) {
+         const responseBody = await response.json();
+         return responseBody;
+      } else {
+         console.error("Error creating Trello board:", response.statusText);
+         return false;
+      }
+   } catch (error) {
+      console.error("Error:", error);
+      return false;
+   }
+};
+
+export const addList = async (name, idBoard) => {
+   const apiURL = `https://api.trello.com/1/lists?key=${apiKey}&token=${apiToken}&name=${name}&idBoard=${idBoard}`;
+   try {
+      const response = await fetch(apiURL, { method: "POST" });
+
+      if (response.ok) {
+         const responseBody = await response.json();
+         return responseBody;
+      } else {
+         console.error("Error creating Trello board:", response.statusText);
+         return false;
+      }
+   } catch (error) {
+      console.error("Error:", error);
+      return false;
+   }
+};
+
+export const addCard = async (name, idList) => {
+   const apiURL = `https://api.trello.com/1/cards?key=${apiKey}&token=${apiToken}&name=${name}&idList=${idList}`;
+   try {
+      const response = await fetch(apiURL, { method: "POST" });
+
+      if (response.ok) {
+         const responseBody = await response.json();
+         return responseBody;
+      } else {
+         console.error("Error creating Trello board:", response.statusText);
+         return false;
+      }
+   } catch (error) {
+      console.error("Error:", error);
+      return false;
+   }
+};
+
+export const addAttachment = async (id, videoFileId) => {
+   const apiURL = `https://api.trello.com/1/cards/${id}/attachments?key=${apiKey}&token=${apiToken}`;
+   const fileUrl = await getFileUrl(videoFileId);
+   try {
+      const response = await fetch(apiURL, {
+         method: "POST",
+         headers: { "Content-Type": "application/json" },
+         body: JSON.stringify({
+            url: fileUrl,
+         }),
+      });
+
+      if (response.ok) {
+         const responseBody = await response.json();
+         console.log(responseBody)
+         return responseBody;
+      } else {
+         console.error("Error creating Trello board:", response.statusText);
+         return false;
+      }
+   } catch (error) {
+      console.error("Error:", error);
+      return false;
+   }
+};
+
+export default getBoards
+
+
+// const current_board_id = await getBoardIdByName("test2");
+// console.log(current_board_id, "current_board_id");
+
+// const current_board_lists = await getBoardLists(current_board_id);
+// console.log(current_board_lists, "current_board_lists");
+
+// const current_list_id = await getBoardListIDByName(current_board_id, "New List");
+// console.log(current_list_id, "current_list_id");
+
+// const newBoard = await addBoard("New Board Name1");
+// console.log(newBoard);
+
+// const newList = await addList("New List", boardId);
+// console.log(newList);
+
+// const newCard = await addCard("New Card2", '658353eeb369bb03ef922315');
+// console.log(newCard);
+// 6583542d57b5fbf157e73534
+// 6583546ea5a2719e4239b490
+
+// const cardlist = await getCardlist('6583542d57b5fbf157e73534');
+// console.log(cardlist);
+
+// const attach = await addAttachment(
+//    "6583542d57b5fbf157e73534",
+//    "BAACAgUAAxkBAANpZYKziL4etIJeLaHVwqN37Ytg_jIAAvYMAAIxFBlUVqZGKyrXpHUzBA"
+// );
+// console.log(attach);
+
+
---
+++ index.js
@@ -0,0 +1,267 @@
+import TelegramBot from "node-telegram-bot-api";
+import fetch from "node-fetch";
+import dotenv from "dotenv";
+dotenv.config();
+import {
+   getBoards,
+   getBoardIdByName,
+   getBoardLists,
+   getBoardListIDByName,
+   getCardlist,
+   getFileUrl,
+   addBoard,
+   addCard,
+   addList,
+   addAttachment
+} from "./helper.js";
+
+// Create a telegram bot with its API KEY
+const token = process.env.TELEGRAM_BOT_TOKEN;
+const bot = new TelegramBot(token, { polling: true });
+
+// class - define a User Class to experess a chat user.
+class User {
+   constructor(status) {
+      this.status = status;
+      this.nickname = "";
+      this.videoCount = "";
+      this.file_id = "";
+      this.file_name = "";
+   }
+
+   initializeExceptLang(status) {
+      this.status = status;
+      this.nickname = "";
+      this.videoCount = "";
+      this.file_id = "";
+      this.file_name = "";
+   }
+}
+
+// Object - store connected user list.
+const telegram2user = {};
+
+// STEP3 - send video
+const createTrelloCardAndAttachVideo = async (
+   MainBoardName,
+   MainListName,
+   chatId,
+   videoFileId,
+   videoFileName
+) => {
+   // Use Trello API to create a card and attach the video
+   let boardId = await getBoardIdByName(MainBoardName);
+   let current_board_id = "";
+   if (boardId) {
+      current_board_id = boardId;
+   } else {
+      const newBoard = await addBoard(MainBoardName);
+      if (newBoard) {
+         current_board_id = newBoard.id;
+      } else {
+         return {
+            type: "error",
+            msg: `Failed to create new Board - ${MainBoardName}`,
+         };
+      }
+   }
+
+   let listId = await getBoardListIDByName(current_board_id, MainListName);
+   let current_list_id = "";
+   if (listId) {
+      current_list_id = listId;
+   } else {
+      const newList = await addList(MainListName, current_board_id);
+      if (newList) {
+         current_list_id = newList.id;
+      } else {
+         return {
+            type: "error",
+            msg: `Failed to create new List - ${MainListName}`,
+         };
+      }
+   }
+
+   let cardName = videoFileName ?? "New Card";
+   const newCard = await addCard(cardName, current_list_id);
+   console.log(newCard,'newCard');
+   const attach = await addAttachment(newCard.id, videoFileId);
+
+   if (attach) {
+      await bot.sendMessage(chatId, `Trello card created with video attachment! - ${videoFileName}`);
+      return {type:"success",msg:"Trello card created with video attachment"}
+   } else {
+      await bot.sendMessage(chatId, `Failed to create with video attachment! - ${videoFileName}`);
+      return {type:"error",msg:"Failed to create the video."}
+   }
+};
+
+// STEP3 - display Instructions
+function displayInstructions(msg, first = false) {
+   console.log(msg.video, "video");
+   telegram2user[msg.chat.id].file_id = msg?.video?.file_id;
+   telegram2user[msg.chat.id].file_name = msg?.video?.file_name;
+   const text = "What do you need done? Please provide instructions";
+   bot.sendMessage(msg.chat.id, text, {
+      reply_markup: {
+         inline_keyboard: [
+            [
+               {
+                  text: "Send in Request",
+                  callback_data: "Send in Request",
+               },
+            ],
+            [
+               {
+                  text: "Edit Request",
+                  callback_data: "Edit Request",
+               },
+            ],
+         ],
+         resize_keyboard: true,
+         one_time_keyboard: true,
+         force_reply: true,
+      },
+   });
+}
+
+// STEP3 - select Instructions
+async function sendInstructions(msg, data) {
+   telegram2user[msg.chat.id].status = "send_request";
+   console.log(telegram2user[msg.chat.id].file_id, "user file id");
+   switch (data) {
+      case "Send in Request":
+         telegram2user[msg.chat.id].selected_request = "Send in Request";
+         await bot.sendMessage(msg.chat.id, "You Selected: Send in Request");
+         break;
+      default:
+         telegram2user[msg.chat.id].selected_request = "Edit Request";
+         await bot.sendMessage(msg.chat.id, "You Selected: Edit Request");
+         break;
+   }
+   const result = await createTrelloCardAndAttachVideo("Videos", "New List", msg.chat.id, telegram2user[msg.chat.id].file_id, telegram2user[msg.chat.id].file_name);
+}
+
+// STEP2 - select the video count
+function displayVideoCountButtons(msg, first = false) {
+   const text = "Please Select the following.";
+   bot.sendMessage(msg.chat.id, text, {
+      reply_markup: {
+         inline_keyboard: [
+            [
+               {
+                  text: "Variations",
+                  callback_data: "Variations",
+               },
+            ],
+            [
+               {
+                  text: "Localization",
+                  callback_data: "Localization",
+               },
+            ],
+            [
+               {
+                  text: "Remake using current trend",
+                  callback_data: "Remake using current trend",
+               },
+            ],
+            [
+               {
+                  text: "UGC",
+                  callback_data: "UGC",
+               },
+            ],
+         ],
+         resize_keyboard: true,
+         one_time_keyboard: true,
+         force_reply: true,
+      },
+   });
+}
+
+// STEP2 - to select a language from a inline keyboard.
+async function sendSelectVideoCount(msg, data) {
+   telegram2user[msg.chat.id].status = "select_file";
+   switch (data) {
+      case "Variations":
+         telegram2user[msg.chat.id].videoCount = "videoCount";
+         await bot.sendMessage(msg.chat.id, "You Selected: videoCount");
+         break;
+      case "Localization":
+         telegram2user[msg.chat.id].videoCount = "Localization";
+         await bot.sendMessage(msg.chat.id, "You Selected: Localization");
+         break;
+      case "Remake using current trend":
+         telegram2user[msg.chat.id].videoCount = "Remake using current trend";
+         await bot.sendMessage(
+            msg.chat.id,
+            "You Selected: Remake using current trend"
+         );
+         break;
+      default:
+         telegram2user[msg.chat.id].videoCount = "UGC";
+         await bot.sendMessage(msg.chat.id, "You Selected: UGC");
+         break;
+   }
+   await bot.sendMessage(
+      msg.chat.id,
+      "please drag n drop the videos you need edited"
+   );
+}
+
+// STEP1 - Greeting
+async function sendGreeting(msg) {
+   await bot.sendMessage(msg.chat.id, "Hello, Attila");
+   await bot.sendMessage(msg.chat.id, "Please add @botExpert03");
+   await bot.sendMessage(msg.chat.id, "How many videos do you need?");
+   displayVideoCountButtons(msg);
+}
+
+// handler - callback after selecting a language from a inline keyboard.
+bot.on("callback_query", (callbackQuery) => {
+   console.log(callbackQuery.message, "call");
+   if (callbackQuery.message.text == "Please Select the following.") {
+      sendSelectVideoCount(callbackQuery.message, callbackQuery.data);
+   }
+   if (
+      callbackQuery.message.text ==
+      "What do you need done? Please provide instructions"
+   ) {
+      sendInstructions(callbackQuery.message, callbackQuery.data);
+   }
+});
+
+bot.on("message", (msg) => {
+   console.log(msg, "msg");
+
+   const user = telegram2user[msg.from.id];
+   const chatId = msg.chat.id;
+
+   try {
+      // ------------------------------------ STEP1 - /start - greeting ----------------------------------------
+      if (msg?.text && msg.text.startsWith("/start")) {
+         telegram2user[msg.from.id] = new User("select_number");
+         console.log(telegram2user, "telegram2user");
+         sendGreeting(msg);
+         return;
+      }
+
+      if (
+         (user.status =
+            "select_file" && Object.keys(msg).indexOf("video") != -1)
+      ) {
+         telegram2user[msg.from.id] = new User("send_request");
+         displayInstructions(msg);
+         return;
+      } else {
+         bot.sendMessage(
+            msg.chat.id,
+            "please drag n drop the videos you need edited"
+         );
+         return;
+      }
+   } catch (e) {
+      console.log(e);
+   }
+});
